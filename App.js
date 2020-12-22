54d30b85a130c986250297766ea0b422a99eef46
Ian Holladay <holladayian@gmail.com>
2020-12-22 10:48:20 -0900

    section out landing page to another file

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import * as React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import LandingPage from './components/LandingPage';
 
 export default function App() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Universal React with butts Expo</Text>
+    <View>
+      <LandingPage />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {color: 'darkslateblue',
-   fontSize: 30,
-    backgroundColor: 'green'
-  }
-})
