3ef7c8cdcce7778a997ccdebaef0e7fde3b62a8b
Ian Holladay <holladayian@gmail.com>
2020-12-22 17:08:12 -0900

    Import and use FlatList

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,22 +1,28 @@
-import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-export default function LandingPage() {
+const LandingPage = () => {
+  const [careRecievers, setRecievers] = useState([
+    {id: 1, name: 'KJ', checkins: [{id: 1.1, checkinName: 'feed the dogs', time: '9:00am', window: "1:00"}, {id: 1.2, checkinName: 'feed the dogs', time: '6:00pm', window: "1:00"}]},
+    {id: 2, name: 'Ian', checkins: [{id: 2.1, checkinName: 'wakeup', time: '7:00am', window: "0:30"}, {id: 2.2, checkinName: 'excersize', time: '8:00am', window: "1:30"}]},
+    {id: 3, name: 'Naomi', checkins: [{id: 3.1, checkinName: 'study', time: '12:00pm', window: "0:15"}, {id: 3.2, checkinName: 'meal prep', time: '6:00pm', window: "0:30"}]}
+  ])
   return (
-    <View style={styles.container}>
+    <>
       <Text style={styles.text}>LandingPage</Text>
-    </View>
+      <FlatList
+        data={careRecievers}
+        renderItem={({ item }) => <Text>{item.name}</Text>} 
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
   text: {color: 'darkslateblue',
    fontSize: 30,
     backgroundColor: 'green'
   }
 })
+
+export default LandingPage;
