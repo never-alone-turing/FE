bfef9e6546b4a204f46290ba1a46440bbeaa3bd6
Ian Holladay <holladayian@gmail.com>
2020-12-22 21:20:48 -0900

    Basic styles for ListItem

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,11 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
-import ListItem from './components/ListItem';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const ListItem = ({item}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
-        <Text style={styles.listItemText}>{item}</Text>
+        <Text style={styles.listItemText}>{item.title}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -16,12 +15,14 @@ const styles = StyleSheet.create({
   listItem: {
     padding: 15,
     backgroundColor: '#f8f8f8',
-    borderBottomWidth: 1,
+    borderBottomWidth: 1,   
     borderColor: "#eee"
   },
   listItemView: {
-    flexDirection: 'row',
-
+    flexDirection: 'row'
+  },
+  listItemText: {
+    fontSize: 18
   }
 })
 
