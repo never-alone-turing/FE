8c36604a16ebb81ab7c2b99d1cbb2a2a4ca008bb
Ian Holladay <holladayian@gmail.com>
2020-12-23 21:55:04 -0900

    Add Receiver.js file for testing react-router-native

---
+++ components/Receiver.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Receiver = () => {
+  return (
+    <View>
+      <Text>Receiver</Text>
+    </View>
+  );
+}
+
+export default Receiver;
