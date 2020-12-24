2fb6138e250439832352bdd45f102c88523f25d0
Ian Holladay <holladayian@gmail.com>
2020-12-23 22:25:10 -0900

    Add 'back' button to Receiver to return to the homepage

--- components/Receiver.js
+++ components/Receiver.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-const Receiver = () => {
+const Receiver = ({history}) => {
   return (
-    <View>
-      <Text>Receiver</Text>
-    </View>
+    <>
+      <Text>Care Receiver</Text>
+      <TouchableOpacity style={styles.listItem} onPress={() => history.push('/')}>
+        <View style={styles.listItemView}>
+          <Text>Back</Text>
+        </View>
+      </TouchableOpacity>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,   
+    borderColor: "#eee"
+  },
+  listItemView: {
+    flexDirection: 'row'
+  },
+  listItemText: {
+    fontSize: 18
+  }
+})
 
 export default Receiver;
