76125eea85f37ed2f15930c5f674a57a25063773
Ian Holladay <holladayian@gmail.com>
2021-01-04 05:27:11 -0900

    Spacing

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { NativeRouter, Switch, Route } from 'react-router-native';
 
 const App = () => {
   // const [careRecievers, setRecievers] = useState([
-  //   {id: 1,name: 'KJ', checkins: [{id: 1.1, checkinName: 'feed the dogs', time: '9:00am', window: "1:00"}, {id: 1.2, checkinName: 'feed the dogs', time: '6:00pm', window: "1:00"}]},
+  //   {id: 1, name: 'KJ', checkins: [{id: 1.1, checkinName: 'feed the dogs', time: '9:00am', window: "1:00"}, {id: 1.2, checkinName: 'feed the dogs', time: '6:00pm', window: "1:00"}]},
   //   {id: 2, name: 'Ian', checkins: [{id: 2.1, checkinName: 'wakeup', time: '7:00am', window: "0:30"}, {id: 2.2, checkinName: 'excersize', time: '8:00am', window: "1:30"}]},
   //   {id: 3, name: 'Naomi', checkins: [{id: 3.1, checkinName: 'study', time: '12:00pm', window: "0:15"}, {id: 3.2, checkinName: 'meal prep', time: '6:00pm', window: "0:30"}]}
   // ])
