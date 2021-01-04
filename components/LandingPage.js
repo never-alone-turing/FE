7a4f8ec8cd51b813b3a470307195aaf88e3cac84
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2021-01-04 16:17:39 -0500

    Refactor to use react-navigation

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,32 +1,63 @@
 import React, {useState} from "react";
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";
-import { NativeRouter, Switch, Route } from 'react-router-native';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from "react-native";
+// import { NativeRouter, Switch, Route } from 'react-router-native';
 import ListItem from './ListItem';
 
-const LandingPage = ({history}) => {
-  const [titles, chooseTitle] = useState([
-    {title: "Caretaker", path: '/caretaker'},
-    {title: "Care Receiver", path: '/carereceiver'}
-  ])
+
+// const LandingPage = ({ history }) => {
+//   const [titles, chooseTitle] = useState([
+//     {title: "Caretaker", path: '/caretaker'},
+//     {title: "Care Receiver", path: '/carereceiver'}
+//   ])
+//   return (
+//     <>
+//       <Text style={styles.text}>Never Alone</Text>
+//         <FlatList
+//           style={styles.accountButton}
+//           data={titles}
+//           keyExtractor={(item) => item.title}
+//           renderItem={({ item }) => 
+//             <ListItem 
+//               item={item} history={history} />} 
+//         />
+//     </>
+//   );
+// }
+
+const LandingPage = ({ navigation }) => {
   return (
-    <>
-      <Text style={styles.text}>Welcome</Text>
-        <FlatList
-          data={titles}
-          renderItem={({ item }) => <ListItem item={item} history={history}/>} 
-        />
-    </>
-  );
+    <View>
+      <Text style={styles.text}>Never Alone</Text>
+      <Button 
+        title="Caretaker"
+        onPress={() => navigation.navigate('Caretaker')}
+      />
+        <Button 
+        title="Receiver"
+        onPress={() => navigation.navigate('Receiver')}
+      />
+    </View>
+    
+  )
 }
 
 const styles = StyleSheet.create({
-  text: {color: 'darkslateblue',
+  text: {
+   color: 'darkslateblue',
    fontSize: 30,
    alignSelf: "stretch",
    textAlign: "center",
    margin: 45
     // backgroundColor: 'green'
+  },
+  accountButton: {
+   color: 'black',
+   borderColor: 'black',
+   margin: 30,
+   padding: 10,
+   fontSize: 30
   }
+
 })
 
 export default LandingPage;
