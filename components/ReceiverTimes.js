c79fa5977afd8601b9a817eebeeefd3ade0abb77
Ian Holladay <holladayian@gmail.com>
2021-01-05 15:18:52 -0900

    Add ReceiverTimes, so a receiver can see when their next checkins are

---
+++ components/ReceiverTimes.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+
+const Times = ({item}) => {
+  return (
+        <View style={styles.listItemView}>
+          <Text style={styles.listItemName}>Name: {item.name}</Text>
+          <Text style={styles.listItemName}>Time: {item.time}</Text>
+          <Text style={styles.listItemName}>Window: {item.window}</Text>
+        </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,   
+    borderColor: "#eee"
+  },
+  listItemView: {
+    flexDirection: 'column'
+  },
+  listItemName: {
+    fontSize: 18
+  },
+  listItemTime: {
+    fontSize: 18,
+  },
+  closeButton: {
+    fontSize: 22,
+    color: 'firebrick',
+    backgroundColor: 'black'
+  }
+})
+
+export default Times;
