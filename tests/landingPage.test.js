bb31b5e21f50e707a3bfb558e37055c298e0ddf0
Naomi Ware <nware1066@yahoo.com>
2021-01-08 12:10:33 -0700

    Change format for LandingPage tests

--- tests/landingPage.test.js
+++ tests/landingPage.test.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {create} from 'react-test-renderer'
 import LandingPage from '../components/LandingPage'
+import renderer from 'react-test-renderer'
 
 
-const tree = create(<LandingPage />);
-
-test('snapshot', () => {
-  expect(tree).toMatchSnapshot();
-});
+  describe("<LandingPage/>", () => {
+    it('renders correctly', () => {
+      const tree = renderer.create(<LandingPage />).toJSON();
+    expect(tree).toMatchSnapshot()
+    })
+  })
+// it('renders correctly', () => {
+//   const tree = renderer.create(<App />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
 //
