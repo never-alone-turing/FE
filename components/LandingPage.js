fb5591dd07ac165dafa6f71c17add629b4d98566
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2021-01-09 19:01:52 -0500

    Remove unused code

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,29 +1,6 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
-import { Button, Header } from 'react-native-elements';
-// import { NativeRouter, Switch, Route } from 'react-router-native';
-import ListItem from './ListItem';
-
-
-// const LandingPage = ({ history }) => {
-//   const [titles, chooseTitle] = useState([
-//     {title: "Caretaker", path: '/caretaker'},
-//     {title: "Care Receiver", path: '/carereceiver'}
-//   ])
-//   return (
-//     <>
-//       <Text style={styles.text}>Never Alone</Text>
-//         <FlatList
-//           style={styles.accountButton}
-//           data={titles}
-//           keyExtractor={(item) => item.title}
-//           renderItem={({ item }) =>
-//             <ListItem
-//               item={item} history={history} />}
-//         />
-//     </>
-//   );
-// }
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Header } from 'react-native-elements';
 
 const LandingPage = ({ navigation }) => {
   return (
@@ -34,19 +11,10 @@ const LandingPage = ({ navigation }) => {
           leftComponent={{ icon: 'home', color: '#fff', size: 40, onPress:() => navigation.navigate('Home')} }
         />
       <Text style={styles.text}>Choose Your Role:</Text>
-
-
-      <TouchableOpacity
-        style={styles.careTakerButton}
-        onPress={() => navigation.navigate('Caretaker')}
-      >
+      <TouchableOpacity style={styles.careTakerButton} onPress={() => navigation.navigate('Caretaker')} >
         <Text style={styles.buttonText}>Caretaker</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.receiverButton}
-        onPress={() => navigation.navigate('Receiver')}
-      >
+      <TouchableOpacity style={styles.receiverButton} onPress={() => navigation.navigate('Receiver')} >
         <Text style={styles.buttonText}>Receiver</Text>
       </TouchableOpacity>
     </View>
@@ -61,7 +29,6 @@ const styles = StyleSheet.create({
    alignSelf: "stretch",
    textAlign: "center",
    margin: 45
-    // backgroundColor: 'green'
   },
 
   careTakerButton: {
@@ -73,9 +40,6 @@ const styles = StyleSheet.create({
    padding: 10,
    fontSize: 30,
    elevation: 8
-   // borderStyle: "solid",
-   // borderColor: "black",
-   // borderWidth: 3
  },
 
  receiverButton: {
@@ -87,9 +51,6 @@ const styles = StyleSheet.create({
   padding: 10,
   fontSize: 30,
   elevation: 8
-  // borderStyle: "solid",
-  // borderColor: "black",
-  // borderWidth: 3
  },
 
  buttonText: {
