9c56724656c3abcf81ad9df147117c6cb67975a8
Ian Holladay <holladayian@gmail.com>
2021-01-10 16:12:22 -0900

    Add api methods to return all timers, delete a timer, and add a timer

--- API/API.js
+++ API/API.js
@@ -1,44 +1,163 @@
-export const apiCall = (data) => {
-  fetch("https://never-alone-backend.herokuapp.com/graphql", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query: `
-    mutation
-      {
-        updateCheckin( 
-          input: {
-            id: "1",
-            response: "Answered"
-          }
-        ) {
-          checkin{
-            id
-            response
-            name
-            userId
-            groupId
-            time
-            window
-          }
-        }
-      }`
-    }),
-  })
-  .then(res => res.json())
-  .then(res => console.log(res.data));
-}
 // export const apiCall = (data) => {
-//   fetch('https://never-alone-backend.herokuapp.com/graphql', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
+//   fetch("https://never-alone-backend.herokuapp.com/graphql", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
 //     body: JSON.stringify({ query: `
+//     mutation
 //       {
-//         allCheckins{
-//           id
+//         updateCheckin( 
+//           input: {
+//             id: "1",
+//             response: "Answered"
+//           }
+//         ) {
+//           checkin{
+//             id
+//             response
+//             name
+//             userId
+//             groupId
+//             time
+//             window
+//           }
 //         }
 //       }`
 //     }),
 //   })
 //   .then(res => res.json())
 //   .then(res => console.log(res.data));
+// }
+export const fetcher = {
+  allTimers() {
+    return fetch("https://never-alone-backend.herokuapp.com/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: `
+        {
+          allCheckins{
+            id
+            name
+            time
+            window
+          }
+        }`
+      }),
+    })
+    .then(res => res.json())
+    .then(res => res.data)
+    // .then(res => console.log("res type", typeof res));
+  },
+
+  deleteTimer(checkinId) {
+    return fetch("https://never-alone-backend.herokuapp.com/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: `
+      mutation{
+        destroyCheckin(
+          input:{
+            id: "${checkinId}"
+          }
+        )
+        {
+          checkin{
+            id
+          }
+        }
+      }`
+      }),
+    })
+    .then(res => res.json())
+    // .then(res => res.data)
+    // .then(res => console.log("res type", typeof res));
+  },
+
+  addTimer(timer) {
+    return fetch("https://never-alone-backend.herokuapp.com/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: `
+      mutation{
+        addCheckin(input:{
+          response: "Pending",
+          time: "${timer.time}",
+          window: "${timer.window}",
+          userId: "1",
+          groupId: "1",
+          name: "${timer.name}"
+          }
+          ){
+            checkin{
+              response
+              time
+              window
+              userId
+              groupId
+              name
+            }
+          }
+        }`
+      }),
+    })
+    .then(res => res.json())
+    // .then(res => res.data)
+    // .then(res => console.log("res type", typeof res));
+  },
+  // deleteTimer(userId) {
+  //   return fetch("https://never-alone-backend.herokuapp.com/graphql", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ query: `
+  //     mutation
+  //       {
+  //         destroyUser(
+  //           input:{
+  //             id: "${userId}"
+  //           }
+  //         )
+  //       }`
+  //     }),
+  //   })
+  //   .then(res => res.json())
+  //   // .then(res => res.data)
+  //   // .then(res => console.log("res type", typeof res));
+  // },
+  // allTimers() {
+  //   return fetch("https://never-alone-backend.herokuapp.com/graphql", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ query: `
+  //       {
+  //         allCheckins{
+  //           id
+  //           name
+  //           time
+  //           window
+  //         }
+  //       }`
+  //     }),
+  //   })
+  //   .then(res => res.json())
+  //   .then(res => res.data)
+  //   // .then(res => console.log("res type", typeof res));
+  // },
+} 
+// export const apiCall = () => {
+//   return fetch("https://never-alone-backend.herokuapp.com/graphql", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ query: `
+//       {
+//         allCheckins{
+//           id
+//           name
+//           time
+//           window
+//         }
+//       }`
+//     }),
+//   })
+//   .then(res => res.json())
+//   .then(res => res.data['allCheckins'])
+//   .then(res => console.log("res type", typeof res));
 // }
