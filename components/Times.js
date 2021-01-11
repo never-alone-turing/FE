fa63ccb6e5dd495ac64462d44ef38aec7212174b
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2021-01-11 18:10:04 -0500

    Add bold font weight to task property titles

--- components/Times.js
+++ components/Times.js
@@ -8,9 +8,9 @@ const Times = ({item, deleteTimer}) => {
         <View style={styles.listItemView}>
           <Button title='X' buttonStyle={styles.closeButton} titleStyle={styles.closeButtonText} onPress={() => deleteTimer(item.id)}/>
           <Text style={item.response === "Answered" ? styles.answered : styles.unanswered}>Status: {item.response === "Answered" ? "Checked In" : "Not Checked In"}</Text>
-          <Text style={styles.listItemName}>Name: {item.name}</Text>
-          <Text style={styles.listItemName}>Time: {new Date(item.time).getHours().toString()}:{new Date(item.time).getMinutes().toString()}</Text>
-          <Text style={styles.listItemName}>Window: {new Date(item.window).getHours().toString()}:{new Date(item.window).getMinutes().toString()}</Text>
+          <Text style={styles.listItemName}><Text style={styles.listItemTitle}>Name: </Text> {item.name}</Text>
+          <Text style={styles.listItemName}><Text style={styles.listItemTitle}>Time: </Text> {new Date(item.time).getHours().toString()}:{new Date(item.time).getMinutes().toString()}</Text>
+          <Text style={styles.listItemName}><Text style={styles.listItemTitle}>Window: </Text> {new Date(item.window).getHours().toString()}:{new Date(item.window).getMinutes().toString()}</Text>
           
         </View>
       </TouchableOpacity>
@@ -27,20 +27,25 @@ const styles = StyleSheet.create({
     borderColor: "#eee"
   },
   answered: {
-    padding: normalize(15),
+    padding: normalize(10),
     backgroundColor: '#69c986',
     borderBottomWidth: normalize(1),   
     borderColor: "#eee",
     marginVertical: normalize(5),
-    fontSize: normalize(20)
+    fontSize: normalize(25),
+    // fontWeight: "bold"
   },
   unanswered: {
-    padding: normalize(7),
+    padding: normalize(10),
     backgroundColor: '#f5a3af',
     borderBottomWidth: normalize(1),   
     borderColor: "#eee",
     marginVertical: normalize(5),
-    fontSize: normalize(20)
+    fontSize: normalize(25),
+    // fontWeight: "bold"
+  },
+  listItemTitle: {
+    fontWeight: "bold"
   },
   listItemView: {
     flexDirection: 'column'
