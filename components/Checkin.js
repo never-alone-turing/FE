75ae582e4d5328c351ed0a126908d2ff821cb2f7
Ian Holladay <holladayian@gmail.com>
2021-01-10 21:14:58 -0900

    Add methods to check if time is checkinable

--- components/Checkin.js
+++ components/Checkin.js
@@ -3,8 +3,8 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import normalize from 'react-native-normalize';
 
 class Checkin extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       currentStyle: styles.mainBtn
     }
@@ -19,9 +19,52 @@ class Checkin extends React.Component {
     this.forceUpdate()
   }
 
+  //
+  returnMinutes = (timePiece) => {
+    var hours = new Date(timePiece).getHours();
+    var minutes = new Date(timePiece).getMinutes();
+    console.log("typeof hours", typeof hours)
+    console.log("typeof minutes", typeof minutes)
+    return hours*60+minutes*1;
+  }
+
+  // twentyFour = () => {
+  //   if(typeof timePiece === 'string') {
+  //     var time = timePiece.split(':');
+  //     return time[0]*60+time[1]*1;
+  //   }
+  // }
+  // function getMinutes(str) {
+  //   var time = str.split(':');
+  //   return time[0]*60+time[1]*1;
+  // }
+
+  getMinutesNow = () => {
+    var timeNow = new Date();
+    return timeNow.getHours()*60+timeNow.getMinutes();
+  }
+
+  checkinable = () => {
+    var now = this.getMinutesNow();
+    var start = this.returnMinutes(this.props.task.time);
+    var end = this.returnMinutes(this.props.task.window);
+    console.log("this.props.task", this.props.task)
+    console.log("start", start)
+    console.log("end", end)
+    if (start > end) end += 1440;
+  
+    if ((now > start) && (now < end)) { 
+      return styles2.mainBtn// your code here
+    } else {
+      return styles.mainBtn // your code here
+    }
+  }
+  //
+
+
   render() {
     return (
-      <TouchableOpacity style={this.state.currentStyle} onLongPress={() => this.changeDecider()}>
+      <TouchableOpacity style={this.checkinable()} onLongPress={() => this.checkinable()}>
         <View>
           <Text style={styles.buttonText}>Hold to complete task.</Text>
         </View>
