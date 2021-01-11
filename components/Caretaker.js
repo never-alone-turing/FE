213a4efa0f052c6979c0bf88bc09e3b07aa7c644
Ian Holladay <holladayian@gmail.com>
2021-01-10 16:13:14 -0900

    Turn Caretaker into a class component and call fetch methods to udate state

--- components/Caretaker.js
+++ components/Caretaker.js
@@ -5,58 +5,111 @@ import Times from './Times';
 import AddTask from './AddTask';
 import normalize from 'react-native-normalize';
 import { ScrollView } from "react-native-gesture-handler";
+import {fetcher} from '../API/API.js';
 
-const Caretaker = ({history, navigation}) => {
-  const [timers, editTimers] = useState([
-    {id: 1, name: "Wake-up", time: new Date(), window: new Date()},
-    {id: 2, name: "Take meds", time: new Date(), window: new Date()},
-    {id: 3, name: "go to sleep", time: new Date(), window: new Date()}
-  ])
-  const deleteTimer = (id) => {
-    editTimers(prevTimers => {
-      return prevTimers.filter(timer => timer.id != id)
-    })
+
+class Caretaker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timers: [],
+      modalVisible: false
+    }
+  }
+  // const [timers, editTimers] = useState([
+  //   {id: 1, name: "Wake-up", time: new Date(), window: new Date()},
+  //   {id: 2, name: "Take meds", time: new Date(), window: new Date()},
+  //   {id: 3, name: "go to sleep", time: new Date(), window: new Date()}
+  // ])
+
+  // async componentDidMount() {
+  //   let timers = await apiCall()
+  //   console.log("timers 27 caretaker", timers)
+  //   // this.setState({ timers: timers.allCheckins })
+  // }
+
+  componentDidMount = () => {
+    this.updateTimes()
+  }
+  
+  updateTimes = async() => {
+    try {
+      const timers = await fetcher.allTimers()
+      console.log("timers 33 caretaker", timers['allCheckins'])
+      this.setState({ timers: timers.['allCheckins'] })
+  
+    } catch(error) {
+      console.log("FUCK", error)
+    }
   }
 
-  const addTimer = (timer) => {
-    console.log('addTimer from Caretaker.js')
-    editTimers(prevTimers => {
-      return [
-        {
-          id: Date.now(),
-          name: timer.name,
-          time: timer.time,
-          window: timer.window
-        }, ...prevTimers]
-    })
+// const Caretaker = ({history, navigation}) => {
+//   const [timers, editTimers] = useState([
+//     {id: 1, name: "Wake-up", time: new Date(), window: new Date()},
+//     {id: 2, name: "Take meds", time: new Date(), window: new Date()},
+//     {id: 3, name: "go to sleep", time: new Date(), window: new Date()}
+//   ])
+
+  deleteTimer = async(id) => {
+    console.log("Delete id", id, typeof id)
+    await fetcher.deleteTimer(id)
+    this.updateTimes()
+    this.forceUpdate()
   }
 
-  const [modalVisible, setIsVisible] = useState(false);
+  // deleteTimer = (id) => {
+  //   editTimers(prevTimers => {
+  //     return prevTimers.filter(timer => timer.id != id)
+  //   })
+  // }
 
-  return (
-    <View>
-      <Header
-        containerStyle={{ backgroundColor: 'black', justifyContent: 'space-between' }}
-        centerComponent={{ text: 'Never Alone', style: { color: '#fff', fontSize: normalize(30), fontWeight: "bold" } }}
-        leftComponent={{ icon: 'home', color: '#fff', size: normalize(40), onPress:() => navigation.navigate('Home')} }
-        rightComponent={{ icon: 'add', color: '#fff', size: normalize(40), onPress:() => setIsVisible(true)} }
-      />
+  addTimer = async(timer) => {
+    await fetcher.addTimer(timer)
+    this.updateTimes()
+    // console.log('addTimer from Caretaker.js')
+    // editTimers(prevTimers => {
+    //   return [
+    //     {
+    //       id: Date.now(),
+    //       name: timer.name,
+    //       time: timer.time,
+    //       window: timer.window
+    //     }, ...prevTimers]
+    // })
+  }
+
+  setIsVisible = (visVal) => {
+    this.setState({ modalVisible: visVal })
+  }
+
+  // const [modalVisible, setIsVisible] = useState(false);
+
+  render() {
+    return (
       <View>
-      <FlatList
-          data={timers}
-          renderItem={({ item }) => (
-            <Times item={item} deleteTimer={deleteTimer}/>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />  
+        <Header
+          containerStyle={{ backgroundColor: 'black', justifyContent: 'space-between' }}
+          centerComponent={{ text: 'Never Alone', style: { color: '#fff', fontSize: normalize(30), fontWeight: "bold" } }}
+          leftComponent={{ icon: 'home', color: '#fff', size: normalize(40), onPress:() => this.props.navigation.navigate('Home')} }
+          rightComponent={{ icon: 'add', color: '#fff', size: normalize(40), onPress:() => this.setIsVisible(true)} }
+        />
+        <View>
+        <FlatList
+            data={this.state.timers}
+            renderItem={({ item }) => (
+              <Times item={item} deleteTimer={this.deleteTimer}/>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          />  
+        </View>
+        <Modal visible={this.state.modalVisible} animationType="slide">
+          <Button buttonStyle={styles.buttonStyle} title='Close Menu' titleStyle={styles.titleStyle} onPress={() => this.setIsVisible(!this.state.modalVisible)}></Button>
+          <AddTask addTimer={this.addTimer} setIsVisible={this.setIsVisible}/>
+        </Modal>
+  
       </View>
-      <Modal visible={modalVisible} animationType="slide">
-        <Button buttonStyle={styles.buttonStyle} title='Close Menu' titleStyle={styles.titleStyle} onPress={() => setIsVisible(!modalVisible)}></Button>
-        <AddTask addTimer={addTimer} setIsVisible={setIsVisible}/>
-      </Modal>
-
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
