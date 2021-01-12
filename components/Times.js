01e9369d5c51e517ea656dba5de6ffa42ac182d7
Naomi Ware <nware1066@yahoo.com>
2021-01-11 18:45:44 -0700

    Change delete button to allow font color change

--- components/Times.js
+++ components/Times.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import normalize from 'react-native-normalize'
 
 const Times = ({item, deleteTimer}) => {
   return (
       <TouchableOpacity style={styles.listItem} >
         <View style={styles.listItemView}>
-          <Button title='X' style={styles.closeButton} onPress={() => deleteTimer(item.id)}/>
+          <TouchableOpacity title='X' style={styles.closeButton} onPress={() => deleteTimer(item.id)}>
+            <Text style={styles.closeButton}>X</Text>
+          </ TouchableOpacity>
           <Text style={styles.listItemName}>Name: {item.name}</Text>
           <Text style={styles.listItemName}>Time: {new Date(item.time).getHours().toString()}:{new Date(item.time).getMinutes().toString()}</Text>
           <Text style={styles.listItemName}>Window: {new Date(item.window).getHours().toString()}:{new Date(item.window).getMinutes().toString()}</Text>
@@ -18,11 +21,11 @@ const styles = StyleSheet.create({
   listItem: {
     padding: 15,
     backgroundColor: '#f8f8f8',
-    borderBottomWidth: 1,   
-    borderColor: "#eee"
+    borderBottomWidth: 2,
+    borderColor: "darkgrey"
   },
   listItemView: {
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   listItemName: {
     fontSize: 18
@@ -31,9 +34,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   closeButton: {
-    fontSize: 22,
-    color: 'firebrick',
-    backgroundColor: 'black'
+    fontSize: normalize(30),
+    textAlign: "center",
+    color: 'indianred',
+    backgroundColor: 'darkslateblue'
   }
 })
 
